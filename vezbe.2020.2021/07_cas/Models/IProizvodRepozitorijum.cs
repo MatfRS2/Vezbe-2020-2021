f970a25453fd3c9eea159af933961a3cbf3a847c
Vladimir Filipovic <vladofilipovic@hotmail.com>
2024-11-30 15:37:50 +0100

    Revert "popravio"
    This reverts commit a463bb356bcbb919e9368048e3abaaa8d47944ec.

---
+++ vezbe.2020.2021/07_cas/Models/IProizvodRepozitorijum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Prodavnica2.Models
+{
+    public interface IProizvodRepozitorijum
+    {
+        IQueryable<Proizvod> Proizvodi { get; }
+
+        Proizvod BrisiProizvod(int proizvodID);
+        Task SacuvajProizvod(Proizvod proizvod);
+    }
+}
